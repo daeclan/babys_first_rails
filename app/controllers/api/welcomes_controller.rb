22c7632b52f22cd8f2129ef02864eb8ae99a1a76
Declan Murphy <declxn@gmail.com>
2020-07-02 20:13:55 -0500

    Displayed my favorite language, per the exercise.

--- app/controllers/api/welcomes_controller.rb
+++ app/controllers/api/welcomes_controller.rb
@@ -1,5 +1,12 @@
 class Api::WelcomesController < ApplicationController
   def hello
+    @random = Random.new.rand(100)
     render 'hello.json.jb'
   end
+
+  def about 
+    @language = "Love"
+    render 'about.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/welcomes" => "welcomes#hello"
+    get "/about" => "welcomes#about"
   end
 end
